664ba2f0529640c06961eddeef0ac69decbca208
Rafael Coelho <rafaellcoellho@gmail.com>
2019-04-13 23:30:15 -0300

    Quase lá

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
+const axios = require('axios');
+const fs = require('fs');
 
-console.log("Teste")
+const decifra = (string, shift) => {
+  const strLowerCase = string.toLowerCase();
+  const strDeciphered = strLowerCase.split('').map(char => {
+    if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+      const value = char.charCodeAt(0)-97;
+      return String.fromCharCode(97 + ((value - shift )% 26) );
+    } else return char;
+  });
+  return strDeciphered.join('');
+};
+
+console.log(decifra('xkgroze ozykrl oy zuu uhbouay zu hk zxak. pkgt hgajxorrgxj', 6));
+
+// console.log("Começa requisição...");
+// axios.get("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=0435cde572fdbd58a23c7abc7ea52e22af0e03ae")
+//   .then(ret => {
+//     const message = ret.data;
+
+//     console.log("Cria arquivo awswer.json");
+//     fs.writeFileSync("answer.json", JSON.stringify(message));
+
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   })
+//   .then(_ => {
+//     console.log("Apaga arquivo answer.json");
+//     fs.unlinkSync("answer.json");
+//     console.log("Fim do Programa!");
+//   })
